7844c9035b5338ac7268a6a7c41410653bde9ad8
mubeel <muhammadmdgh1@gmail.com>
2025-05-10 02:08:32 +0300

    creating a html and css form and getting the elemt in js file

---
+++ main.js
@@ -0,0 +1,10 @@
+// import _ from'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@';
+
+const name = document.querySelector('#input-name');
+const qUnumber = document.querySelector('#input-add-num');
+const Phone = document.querySelector('#phone-number');
+const collage = document.querySelector('#collage');
+
+function Submit() {
+    console.log("it's works");
+}
